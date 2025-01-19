e151aaef84d99d8bf0f60371d97dceb828ead2a6
ZiadRefaey <ziad.refaey1@gmail.com>
2025-01-19 09:33:13 +0200

    style: homepage new arrivals section UI

--- app/(customer)/page.tsx
+++ app/(customer)/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedProducts from "@/components/customer/layout/Home/FeaturedProducts";
 import Hero from "@/components/customer/layout/Home/Hero";
+import NewArrivalsProducts from "@/components/customer/layout/Home/NewArrivalsProducts";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <NewArrivalsProducts />
     </>
   );
 }
---
+++ components/customer/layout/Home/NewArrivalsProducts.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { product } from "./product.type";
+import Section from "../../ui/Section";
+import ProductsShowcase from "./ProductsShowcase";
+import SecondaryButton from "../../ui/SecondaryButton";
+const newArrivalsProducts: product[] = [
+  {
+    discountPercentage: 20,
+    href: "/",
+    price: 200,
+    rating: 3.5,
+    title: "JACKET WITH STRIPED SLEEVES",
+  },
+  {
+    discountPercentage: 30,
+    href: "/",
+    price: 400,
+    rating: 4.5,
+    title: "HOODIE WITH ZIP POCKETS",
+  },
+  {
+    discountPercentage: 25,
+    href: "/",
+    price: 180,
+    rating: 3,
+    title: "T-SHIRT WITH TAPE DETAILS",
+  },
+  {
+    discountPercentage: 15,
+    href: "/",
+    price: 150,
+    rating: 4,
+    title: "JEANS WITH DISTRESSED FINISH",
+  },
+];
+export default function NewArrivalsProducts() {
+  return (
+    <Section>
+      <h2 className="text-preset-1 uppercase mb-10">New Arrivals</h2>
+      <ProductsShowcase products={newArrivalsProducts} />
+      <SecondaryButton className="w-[218px] mt-10" href="/">
+        View All
+      </SecondaryButton>
+    </Section>
+  );
+}
