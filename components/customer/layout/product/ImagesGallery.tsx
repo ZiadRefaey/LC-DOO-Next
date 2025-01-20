57adb932e98d17a2df2c75721a78649b73a51e57
ZiadRefaey <ziad.refaey1@gmail.com>
2025-01-20 07:17:39 +0200

    refactor: changing ImagesPanel to ImagesGalary for better clarity

--- components/customer/layout/product/ImagesGallery.tsx
+++ components/customer/layout/product/ImagesGallery.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainImage from "./MainImage";
 import PositionImage from "./PositionImage";
 
-export default function ImagesPanel() {
+export default function ImagesGalary() {
   return (
     <div className="flex items-center justify-center flex-col gap-3 md:gap-5 w-full h-[326px] md:h-[680px] xl:h-[660px]">
       <MainImage title="image" />
