84589eb9bdd6ec24b33ebff00decebdda2c47767
ZiadRefaey <ziad.refaey1@gmail.com>
2025-01-21 06:49:00 +0200

    style: adjusting border with in thumbnails images

--- components/customer/layout/product/PositionImage.tsx
+++ components/customer/layout/product/PositionImage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import image from "@/public/image.png";
 export default function PositionImage() {
   return (
-    <div className="w-full h-full place-items-center bg-card-background flex items-center justify-center rounded-xl cursor-pointer border hover:border-border transition-all duration-150 border-transparent">
+    <div className="w-full h-full place-items-center bg-card-background  flex items-center justify-center rounded-xl cursor-pointer border-2 hover:border-border transition-all duration-150 border-transparent">
       <Image src={image} className="h-[80%] w-auto object-fit" alt="image" />
     </div>
   );
