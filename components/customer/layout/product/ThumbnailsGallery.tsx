d2c49ffd99b22a0b3ed8b4bb2e997c2751103e9f
ZiadRefaey <ziad.refaey1@gmail.com>
2025-01-21 06:23:48 +0200

    style: fixing typo in Gallery

--- components/customer/layout/product/ThumbnailsGallery.tsx
+++ components/customer/layout/product/ThumbnailsGallery.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PositionImage from "./PositionImage";
 
-export default function ThumbnailsGalary() {
+export default function ThumbnailsGallery() {
   return (
     <div className="grid grid-cols-3 w-full h-auto gap-4 place-items-center ">
       <PositionImage />
