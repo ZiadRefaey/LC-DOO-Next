eeb10286ba8d878a6e9451622819e53674104c41
ZiadRefaey <ziad.refaey1@gmail.com>
2025-01-23 13:38:41 +0200

    style: adding max width to actions row

--- components/customer/layout/cart/ItemActions.tsx
+++ components/customer/layout/cart/ItemActions.tsx
@@ -5,7 +5,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 
 export default function ItemActions() {
   return (
-    <div className="grid grid-cols-[0.8fr,0.2fr] gap-2 md:gap-3 w-full">
+    <div className="grid grid-cols-[0.8fr,0.2fr] gap-2 md:gap-3 w-full max-w-[400px]">
       <QuantityPicker className="px-10" />
       <SecondaryButton className="flex items-center justify-center hover:border-error">
         <FaRegTrashAlt className="text-preset-2 text-error" />
